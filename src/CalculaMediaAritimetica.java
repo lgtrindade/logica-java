5f88edb9a7912ed2cfa82a4d4280063bbe7d3ff5
Entra21 <entra21@proway.treina>
2021-05-05 21:32:06 -0300

    Calculadora aritimetica

---
+++ src/CalculaMediaAritimetica.java
@@ -0,0 +1,34 @@
+/*
+Suponha um programa que solicita ao usuario dois numeros e depois mostra a media aritimetica deles:
+
+    Ex:
+        *** ENTRADA DE DADOS ***
+        Digite o primeiro numero: 3
+        Digite o segundo numero: 6
+
+        *** PROCESSAMENTO ***
+
+        MEDIA = (3 + 6) / 2
+
+        *** SAIDA DE DADOS ***
+        media aritimetica: 4.5
+
+
+ */
+
+public class CalculaMediaAritimetica {
+
+    public static void main (String[] args){
+
+        // *** ENTRADA DE DADOS ***
+        double primeiroNumero = 3;
+        double segundoNumero = 6;
+
+        //*** PROCESSAMENTO ***
+        double resultado = (primeiroNumero + segundoNumero) / 2;
+
+        //*** SAIDA DE DADOS ***
+        System.out.println("O resultado e" + resultado);
+
+    }
+}
