7856d1f6150f1a7fa34ffdbd82d480c520681713
Entra21 <entra21@proway.treina>
2021-05-11 20:18:54 -0300

    Exercício 01

---
+++ src/Exercicio01.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+
+public class Exercicio01 {
+
+    public static void main (String[] arg) {
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        byte age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double prince1 = 2100.0;
+        double prince2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $%.2f%n", product1, prince1);
+        System.out.printf("%s, which price is $%.2f%n", product2, prince2);
+        System.out.println();
+        System.out.printf("Record: %d years old, code %d and gender: %c%n ", age, code, gender);
+        System.out.println();
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("Us decimal point: %.3f%n", measure);
+    }
+}
