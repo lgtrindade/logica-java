8d8fd1518ef68e4032c75ad1507b61a3928368f5
Entra21 <entra21@proway.treina>
2021-05-12 19:53:01 -0300

    alClasse de Entrada de dados atualizada

--- src/EntradaDados.java
+++ src/EntradaDados.java
@@ -7,16 +7,41 @@ public class EntradaDados {
         Scanner scanner = new Scanner(System.in);
 
         String variavelSemEspaco = scanner.next();
-
         System.out.println(variavelSemEspaco);
 
         double valorComVirgula = scanner.nextDouble();
+        System.out.println(valorComVirgula);
 
         int valorInteiroSemVirgula = scanner.nextInt();
+        System.out.println(valorInteiroSemVirgula);
 
         char variavelQueRecebeSomenteUmCaracter = scanner.next().charAt(0);
+        System.out.println(variavelQueRecebeSomenteUmCaracter);
 
+        String x;
+        int y;
+        double z;
 
+        x = scanner.next();
+        y = scanner.nextInt();
+        z = scanner.nextDouble();
+
+        System.out.printf("%s %d %f", x, y, z);
+
+        int x1;
+        String s1, s2, s3;
+
+        x1 = scanner.nextInt();
+        scanner.nextLine();
+        s1 = scanner.nextLine();
+        s2 = scanner.nextLine();
+        s3 = scanner.nextLine();
+
+        System.out.println("Dados digitados:");
+        System.out.println(x1);
+        System.out.println("s1 ->" +s1);
+        System.out.println("s2 ->" +s2);
+        System.out.println("s3 ->" +s3);
 
         scanner.close();
 
