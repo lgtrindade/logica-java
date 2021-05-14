1a40e06a3cea1a8ce948e3a6ab70d94e46a6f4ed
Entra21 <entra21@proway.treina>
2021-05-14 18:40:30 -0300

    Exercicio 1003

---
+++ src/Ex1003.java
@@ -0,0 +1,48 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex1003 {
+    public static void main(String[] args) {
+
+        /*
+        --- SOMA SIMPLES ---
+
+      Leia dois valores inteiros, no caso para variáveis A e B. A seguir,
+      calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+      Entrada
+      O arquivo de entrada contém 2 valores inteiros.
+
+      Saída
+      Imprima a mensagem "SOMA" com todas as letras maiúsculas,
+      com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B.
+      Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário,
+      você receberá "Presentation Error".
+
+        Exemplos de Entrada            	Exemplos de Saída
+
+                30                          SOMA = 40
+                10
+
+               -30                         SOMA = -20
+                10
+
+                0                           SOMA = 0
+                0
+        */
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        // *** ENTRADA DE DADOS ***
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        //*** PROCESSAMENTO ***
+        int SOMA = (A + B);
+
+        //*** SAIDA DE DADOS ***
+        System.out.println("SOMA = " + SOMA);
+
+        scanner.close();
+    }
+}
