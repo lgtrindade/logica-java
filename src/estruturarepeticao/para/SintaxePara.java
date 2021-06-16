8a53eca4858b2a9d1528e525c459e16f79446e63
Your Name <you@example.com>
2021-06-15 21:18:13 -0300

    Pasta de estrutura para e exercicios

---
+++ src/estruturarepeticao/para/SintaxePara.java
@@ -0,0 +1,16 @@
+package estruturarepeticao.para;
+
+public class SintaxePara {
+
+    /*
+            Fazer um programa que le um valor inteiro e depois N valores inteiros.
+            Ao final somar os valores lidos;
+
+     */
+
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i <=10 ; i++){
+        }
+    }
+}
