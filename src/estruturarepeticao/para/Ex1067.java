b5e3526c5a4d2b4094f5370ee89748d8f3e405de
Your Name <mayconsouzamendes@gmail.com>
2021-06-16 20:38:21 -0300

    Exercicio 1067

---
+++ src/estruturarepeticao/para/Ex1067.java
@@ -0,0 +1,37 @@
+package estruturarepeticao.para;
+
+import java.util.Scanner;
+
+/*
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X,
+um valor por linha, inclusive o X, se for o caso.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+
+Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+
+Exemplo de Entrada	        Exemplo de Saída
+        8                           1
+                                    3
+                                    5
+                                    7
+ */
+
+
+public class Ex1067 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        for (int i = 1; i <= x; i++){
+
+            if (i%2 != 0){
+                System.out.println(i);
+            }
+        }
+        scanner.close();
+    }
+}
